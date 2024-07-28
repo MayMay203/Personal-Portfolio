6b39cb5499191e4a859e7333463650cc16ce16a0
Nhung <hongnhung16052003@gmail.com>
2024-07-28 17:22:01 +0700

    build UI of the header and hero section

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+const navarLink = document.querySelectorAll('.navbar__link')
+navarLink.forEach(link => {
+    link.addEventListener('click', (e) => {
+        navarLink.forEach(link => {
+            link.classList.remove('navbar__link--active')
+        })
+        e.target.classList.add('navbar__link--active')
+    })
+})
