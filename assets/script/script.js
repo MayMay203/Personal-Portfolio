5e53d72db679912f40dcb43382b5b8c3c745df1d
Nhunglth <nhunglth@d-soft.com.vn>
2026-05-19 18:05:10 +0700

    update my portfolio 2026

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,28 +8,7 @@ navarLink.forEach(link => {
     })
 })
 
-const video = document.querySelector('video')
-let isPlay = false
-const playBtn = document.querySelector('.video__play-btn')
-playBtn.addEventListener('click', () => {
-    if (isPlay) {
-        isPlay = false;
-        video.pause();
-        playBtn.innerHTML=''
-        playBtn.innerHTML = '<img src="./assets/icons/play_video.svg" alt="play-icon">'
-    }
-    else {
-        isPlay = true;
-        video.play();
-        playBtn.innerHTML = ''
-        playBtn.innerHTML='<img src="./assets/icons/pause_video.svg" alt="play-icon" class="video__pause-icon">'
-    }
-})
 
-video.addEventListener('ended', () => {
-    isPlay = false;
-    playBtn.innerHTML = '<img src="./assets/icons/play_video.svg" alt="play-icon">';
-});
 
 const specialDots = document.querySelectorAll('.special__dot')
 const specialItems = document.querySelectorAll('.special-item')
@@ -78,20 +57,7 @@ headerBottom.innerHTML = header.innerHTML
 headerBottomMobile.querySelector('.navbar').innerHTML = header.querySelector('.navbar').innerHTML
 headerBottomMobile.querySelector('.header-bottom__logo').innerHTML = header.querySelector('.logo').innerHTML
 
-const feedbackDots = document.querySelectorAll('.feedback__dot')
-const feedbackItems = document.querySelectorAll('.feedback-item')
-feedbackDots.forEach((dot) => {
-    dot.addEventListener('click', e => {
-        feedbackDots.forEach(dot => {
-            dot.classList.remove('feedback__dot--active')
-        })
-        e.target.classList.add('feedback__dot--active')
-        index = e.target.dataset.index
-        feedbackItems.forEach(feedback => {
-            feedback.style.transform = `translateX(${-(index) * 100}%)`
-        })
-    })
-})
+
 
 const atmosphereDots = document.querySelectorAll('.atmosphere__dot')
 const atmosphereItems = document.querySelectorAll('.atmosphere-item')
